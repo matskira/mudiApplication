6644fed0c592c27ca182e575ef818a7f705518cf
Matheus Mariano Poda <matheus.poda@gmail.com>
2021-07-16 10:52:02 -0300

    Adicionado consulta a base de dados com JPA puro

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,19 +1,29 @@
 package br.com.alura.mvc.mudi.controller;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import br.com.alura.mvc.mudi.mock.PedidoMock;
+import br.com.alura.mvc.mudi.dao.PedidosDAO;
+import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private PedidosDAO dao;
+	
+	
 	@GetMapping("/home")
 	public String home(Model model) {
-		model.addAttribute("listaPedidos", PedidoMock.mockaPedido());
-		
-		
+		model.addAttribute("listaPedidos", dao.consultaTodosPedidos());
 		return "home";
 	}
 }
--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -3,7 +3,23 @@ package br.com.alura.mvc.mudi.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pedido {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	private String nomeProduto;
 	private BigDecimal valorNegociado;
 	private LocalDate dataDaEntrega;
