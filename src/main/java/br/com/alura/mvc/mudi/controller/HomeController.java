2f1f2993b75599dcfbac7273bd51fa85c2a96d64
Matheus Mariano Poda <matheus.poda@gmail.com>
2021-07-16 11:01:37 -0300

    Alterado no HomeController o método home agora é do tipo ModelAndView

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,11 +1,14 @@
 package br.com.alura.mvc.mudi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.alura.mvc.mudi.dao.PedidosDAO;
+import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
@@ -15,8 +18,10 @@ public class HomeController {
 	
 	
 	@GetMapping("/home")
-	public String home(Model model) {
-		model.addAttribute("listaPedidos", dao.findAll());
-		return "home";
+	public ModelAndView home() {
+		List<Pedido> pedidos = dao.findAll();
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("listaPedidos", pedidos);
+		return mv;
 	}
 }
