0bc52ec455d781cc4b90d5c9c8deac89fd34b73f
Matheus Mariano Poda <matheus.poda@gmail.com>
2021-07-16 10:54:11 -0300

    Adicionado consulta a base de dados utilizando a implementação do Spring Data

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,18 +1,11 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.dao.PedidosDAO;
-import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
@@ -23,7 +16,7 @@ public class HomeController {
 	
 	@GetMapping("/home")
 	public String home(Model model) {
-		model.addAttribute("listaPedidos", dao.consultaTodosPedidos());
+		model.addAttribute("listaPedidos", dao.findAll());
 		return "home";
 	}
 }
--- src/main/java/br/com/alura/mvc/mudi/dao/PedidosDAO.java
+++ src/main/java/br/com/alura/mvc/mudi/dao/PedidosDAO.java
@@ -1,24 +1,10 @@
 package br.com.alura.mvc.mudi.dao;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.mvc.mudi.model.Pedido;
 
 @Repository
-public class PedidosDAO {
-
-	@PersistenceContext
-	private EntityManager entityMananger;
-	
-	public List<Pedido> consultaTodosPedidos(){
-		Query query = entityMananger.createQuery("select p from Pedido p", Pedido.class);
-		List<Pedido> pedidos = query.getResultList();
-		return pedidos;
-	}
+public interface PedidosDAO extends JpaRepository<Pedido, Long>{
 }
