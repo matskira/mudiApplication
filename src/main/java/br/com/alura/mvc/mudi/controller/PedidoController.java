2ff2c66860b26a38b4cdacdce567d347621ffb52
Matheus Mariano Poda <matheus.poda@gmail.com>
2021-07-19 14:16:33 -0300

    Criado formulário para novo pedido com bean validations

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -1,5 +1,39 @@
 package br.com.alura.mvc.mudi.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.mvc.mudi.dao.PedidosDAO;
+import br.com.alura.mvc.mudi.dto.RequisicaoNovoPedido;
+import br.com.alura.mvc.mudi.model.Pedido;
+
+@Controller
+@RequestMapping("pedido")
 public class PedidoController {
 
+	@Autowired
+	private PedidosDAO dao;
+	
+	@GetMapping("formulario")
+	public String formulario(RequisicaoNovoPedido requisicao) {
+		return "pedido/formulario";
+	}
+	
+	@PostMapping("novo")
+	public String novo(@Valid RequisicaoNovoPedido requisicao, BindingResult result) {
+		
+		if (result.hasErrors()) {
+			return "pedido/formulario";
+		}
+		
+		Pedido pedido = requisicao.toPedido();
+		dao.save(pedido);
+		return "pedido/formulario";
+	}
 }
